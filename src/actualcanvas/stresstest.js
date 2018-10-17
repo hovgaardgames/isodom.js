b2fd4ef5a6d7700b6a19464cfe02bfcf826fd0fc
Jonas Hovgaard <j@jhovgaard.dk>
2018-10-17 08:47:04 +0200

    Added preload

--- src/actualcanvas/stresstest.js
+++ src/actualcanvas/stresstest.js
@@ -1,17 +1,13 @@
-const cols = 60, rows = 60;
-const cellSize = [148, 88]; // Image is 148x87 tho, but w/e, better than dealing with rounding or editing the image Kappa
+let handleComplete = () => {
+    const cols = 50, rows = 50;
+    const cellSize = [148, 88]; // Image is 148x87 tho, but w/e, better than dealing with rounding or editing the image Kappa
 
-const easelCanvas = document.getElementById('easeljs');
-let stage;
+    const easelCanvas = document.getElementById('easeljs');
+    let stage;
 // const stage = new createjs.StageGL("easeljs", {transparent:true, premultiply:true});
 
-const img = new Image();
-img.src = 'ewww.png';
+    const cellList = [];
 
-const cellList = [];
-
-const img2 = new Image();
-img2.onload = function () {
     const canvasWidth = cols * cellSize[0];
     const canvasHeight = cols * cellSize[1];
     const halfWidth = Math.round(canvasWidth / 2);
@@ -19,32 +15,28 @@ img2.onload = function () {
     const halfCellHeight = Math.round(cellSize[1] / 2);
     easelCanvas.width = canvasWidth;
     easelCanvas.height = canvasHeight;
-    stage = new createjs.StageGL("easeljs", {transparent:true, premultiply:true});
+    stage = new createjs.StageGL("easeljs", {transparent: false , premultiply: true});
     stage.scale = 0.5; // 50% (from 0 to 1)
 
     for (let y = 0; y < rows; y++) {
         const rowStartX = halfWidth - halfCellWidth - (y * halfCellWidth);
 
         for (let x = 0; x < cols; x++) {
-            const cell = new createjs.Bitmap(img2);
+            const cell = new createjs.Bitmap(preload.getResult('black-cell'));
             cellList.push(cell);
             cell.addEventListener('mouseover', function () {
-                cell.image = img;
-                stage.update();
+                cell.image = preload.getResult('red-cell');
+                //stage.update();
                 console.log(`cell over: x: ${x}; y: ${y}`);
             });
-            cell.addEventListener('mouseout', function () {
-                cell.image = img2;
-                stage.update();
-            });
+
             cell.x = rowStartX + x * halfCellWidth;
             cell.y = (halfCellHeight * x) + (halfCellHeight * y);
-
             stage.addChild(cell);
         }
     }
 
-    stage.enableMouseOver(20);
+    stage.enableMouseOver(20000);
     stage.update();
 
     function letTheFunBegin() {
@@ -52,10 +44,10 @@ img2.onload = function () {
         let maxLength = cellList.length;
         const raf = function () {
             if (index > 0) {
-                cellList[index - 1].image = img2;
+                cellList[index - 1].image = preload.getResult('black-cell');
             }
 
-            cellList[index].image = img;
+            cellList[index].image = preload.getResult('red-cell');
             index++;
 
             if (index === maxLength) {
@@ -68,6 +60,13 @@ img2.onload = function () {
         window.requestAnimationFrame(raf);
     }
 
-    letTheFunBegin();
+    //letTheFunBegin();
 };
-img2.src = 'cell.png';
+
+// Preload
+preload = new createjs.LoadQueue(true);
+preload.on("complete", handleComplete);
+preload.loadManifest([
+    {src: "ewww.png", id: "red-cell"},
+    {src: "cell.png", id: "black-cell"},
+]);
